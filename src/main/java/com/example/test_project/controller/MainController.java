60742d2991ab136c98959d0df702801d947eaba0
YeoJeongHun <aofhd0003@naver.com>
2022-04-12 23:39:31 +0900

    JPA적용중

--- src/main/java/com/example/test_project/controller/MainController.java
+++ src/main/java/com/example/test_project/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.example.test_project.controller;
 
+import com.example.test_project.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,8 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class MainController {
 
+//    @Autowired
+    ArticleService articleService;
+
     @RequestMapping(method = RequestMethod.GET, path = "/test")
     public String testRequest() {
         return "hihi";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/doWrite")
+    public String doWrite() {
+        articleService.doWrite("test", "test");
+        return "성공";
     }
 }
---
+++ src/main/java/com/example/test_project/service/ArticleService.java
@@ -0,0 +1,9 @@
+package com.example.test_project.service;
+
+public class ArticleService {
+
+    public String doWrite(String title, String content){
+
+        return "S";
+    }
+}
